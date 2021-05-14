2ae096023b74be8f3824f5fa26f7dfae05d5c40f
Anna Kovalyova <akannka@gmail.com>
2021-05-14 14:50:16 +0300

    added method count_e

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <string>
 
+int count_e(std::string row){
+    int result = 0;
+    for (const auto& c : row){
+        result += c == 'e';
+    }
+    return result;
+
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
