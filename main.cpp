47a7174b35606b3064df6916caf0bd828020f5b2
Anna Kovalyova <akannka@gmail.com>
2021-05-14 15:03:44 +0300

    added method count_e_without_r and main

--- main.cpp
+++ main.cpp
@@ -7,9 +7,22 @@ int count_e(std::string row){
         result += c == 'e';
     }
     return result;
-
 }
+
+int count_e_without_r(const std::string& row){
+    int result=0;
+    std::size_t len = row.length();
+    for(int i = 0; i<len; i++){
+        result += row[i] == 'e';
+        result-=i<len-1 && row[i+1] == 'r';
+    }
+    return result;
+}
+
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::string word;
+    std::cin >> word;
+    std:: cout << count_e(word) << ", " << count_e_without_r(word);
     return 0;
 }
